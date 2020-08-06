3acae7e0a659b3b360613a8dbaf89ccb6a8fd75b
vitor2233 <vitorguilherme2233@gmail.com>
2020-08-05 21:53:10 -0300

    Initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+
+  //Fetch here
+
+  return (
+    <div className="App">
+      <p>Hi</p>
+    </div>
+  );
+}
+
+export default App;
